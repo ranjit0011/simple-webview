eae40a863ad94ff24ea68505ae93c454b0b1ca44
Ranjit <ranjitgoudbiradar@gmail.com>
2023-01-17 09:30:47 +0530

    update

--- app/src/main/java/com/simple/webview/PageofflineActivity.java
+++ app/src/main/java/com/simple/webview/PageofflineActivity.java
@@ -17,7 +17,7 @@ public class PageofflineActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pageoffline);
+       // setContentView(R.layout.activity_pageoffline);
 
         layout=findViewById(R.id.layout);
         btnretry=findViewById(R.id.btnretry);
