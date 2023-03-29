006bc39e042ca7f16450334b77bb76a0fdeed56b
Ranjit <ranjitgoudbiradar@gmail.com>
2023-03-29 09:07:24 +0530

    update

--- app/src/main/java/com/simple/webview/PageofflineActivity.java
+++ app/src/main/java/com/simple/webview/PageofflineActivity.java
@@ -24,7 +24,7 @@ public class PageofflineActivity extends AppCompatActivity {
 
         btnretry.setOnClickListener(new View.OnClickListener() {
             @Override
-            //public void onClick(View v) {
+            public void onClick(View v) {
                 Intent intent = new Intent(PageofflineActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
