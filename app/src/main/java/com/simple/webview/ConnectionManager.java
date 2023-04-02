7ba7191e01b694e40593b32cc9a238e8faaa0462
Ranjit <ranjitgoudbiradar@gmail.com>
2023-04-02 09:42:29 +0530

    update

--- app/src/main/java/com/simple/webview/ConnectionManager.java
+++ app/src/main/java/com/simple/webview/ConnectionManager.java
@@ -8,8 +8,8 @@ public class ConnectionManager {
 
     public static boolean checkconnection(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        //NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        NetworkInfo mobileInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        //NetworkInfo mobileInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 
         if ((wifiInfo != null && wifiInfo.isConnected()) || (mobileInfo != null && mobileInfo.isConnected())) {
             return true;
