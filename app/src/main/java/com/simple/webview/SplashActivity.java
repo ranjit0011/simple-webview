4c4fba2cb37bdb229207203260dfdc14096ec918
Ranjit <ranjitgoudbiradar@gmail.com>
2023-06-12 08:33:50 +0530

    update it

--- app/src/main/java/com/simple/webview/SplashActivity.java
+++ app/src/main/java/com/simple/webview/SplashActivity.java
@@ -20,8 +20,8 @@ public class SplashActivity extends AppCompatActivity {
             new Handler().postDelayed(new Runnable() {
         @Override public void run() {
             Intent i = new Intent(SplashActivity.this, MainActivity.class); startActivity(i);
-            finish(); } }, 3000);
-
+            finish(); } }, 4000);
+            //ProgressBar
         ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
         Sprite doubleBounce = new DoubleBounce();
         progressBar.setIndeterminateDrawable(doubleBounce);
